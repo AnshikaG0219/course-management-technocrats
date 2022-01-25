b236cd7cc0e02b3d3a058cb93ecf00517b1597d9
Anshika Gautam <anshikagautam130@gmail.com>
2022-01-25 12:44:51 +0530

    diff routes defined

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const multer = require("multer");
 const app = express();
-const User = require('./models/user')
-const Course = require("./models/course").Course
-const login = require('./routes/login')
-const register = require('./routes/register')
-const student = require('./routes/student')
-const teacher = require('./routes/teacher')
-const profile = require('./routes/profile');
+const User = require("./models/user");
+const Course = require("./models/course").Course;
+const login = require("./routes/login");
+const register = require("./routes/register");
+const student = require("./routes/student");
+const teacher = require("./routes/teacher");
+const profile = require("./routes/profile");
 const course = require("./models/course");
 
 app.use(express.static(__dirname + "/public"));
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const dir  = './public/uploads/'+file.fieldname+'/';
-    cb(null,dir);
+    const dir = "./public/uploads/" + file.fieldname + "/";
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
@@ -74,193 +74,34 @@ app.get("/:user_id/verify", teacher.verifyGET);
 app.get("/teacher/:teach_id/add-course", teacher.addCourse);
 app.get("/profile/:id", profile.profileGET);
 app.get("/edit-profile/:id", profile.editProfile);
-app.get("/logout", function (req, res) {
-req.logout();
-res.redirect("/login");
-});
-app.get("/delete/:course_ID", function (req, res) {
-  const c_ID = req.params.course_ID;
-  if (req.isAuthenticated()) {
-    Course.deleteOne({_id: c_ID}, function (err) {
-      if (!err) res.redirect(`/teacher/${req.user._id}/courses`);
-    });
-  } else {
-    res.redirect("/login");
-  }
-});
-app.get("/update-course/:course_ID", function (req, res) {
-  if (req.isAuthenticated()) {
-    res.render(`teacher/update-course`, {user: req.user, course_id: req.params.course_ID});
-  } else {
-    res.redirect("/login");
-  }
-});
-app.get("/:user_", function (req, res) {
+app.get("/logout", login.logout);
+app.get("/delete/:course_ID", teacher.deleteCourse);
+app.get("/update-course/:course_ID", teacher.updateCourse);
+app.get("/:user", function (req, res) {
   if (req.user.role === "teacher") res.redirect("/teacher/" + req.user._id);
   else if (req.user.role === "student")
     res.redirect("/student/" + req.user._id);
 });
-app.get("/view-course/:course_id", function (req, res) {
-  let course_id = req.params.course_id;
-  if (req.isAuthenticated()) {
-    Course.find({ _id: course_id }, function (err, c) {
-      res.render(`view-course`, { user: req.user, course: c });
-    });
-    res;
-  } else {
-    res.redirect("/login");
-  }
-});
-app.post("/:user_id/verify", upload.single('identification'), function(req, res){
-  const u_id = req.params.user_id
-  if(req.isAuthenticated())
-  {
-    // User.updateOne({_id: u_id}, {identity: req.file.filename})
-    res.redirect(`/teacher/${u_id}`)
-  }else{
-    res.redirect("/login")
-  }
-})
-app.post("/update/:user_id", function (req, res) {
-  const newName = req.body.name;
-  const newEmail = req.body.name;
-  const ID = req.params.user_id;
-  if (req.isAuthenticated()) {
-    User.updateOne(
-      { _id: ID },
-      { name: newName, username: newEmail },
-      function (err) {
-        if (!err) {
-          if (req.user.role === "teacher") res.redirect(`/teacher/${ID}`);
-          else if (req.user.role === "student") res.redirect(`/student/${ID}`);
-        }
-      }
-    );
-  }
-});
-
-app.post("/teacher/:course_id/update-course-name", function (req, res) {
-    if (req.isAuthenticated()) {
-       Course.findOneAndUpdate({_id: req.params.course_id}, {$set: {title: req.body.title}},{new: true}, function(err,c){
-       if(err) console.log(err);
-       else{
-         res.redirect(`/teacher/${req.user._id}/courses`)
-       }
-     })
-    } else {
-      res.redirect("/login");
-    }
-  }
-);
-
-app.post("/teacher/:course_id/update-course-desc", function (req, res) {
-    if (req.isAuthenticated()) {
-       Course.findOneAndUpdate({_id: req.params.course_id}, {$set: {description: req.body.desc}},{new: true}, function(err,c){
-       if(err) console.log(err);
-       else{
-          res.redirect(`/teacher/${req.user._id}/courses`);
-       }
-     })
-    } else {
-      res.redirect("/login");
-    }
-  }
-);
-
-app.post("/teacher/:course_id/update-course-price", function (req, res) {
-    if (req.isAuthenticated()) {
-       Course.findOneAndUpdate({_id: req.params.course_id}, {$set: {price: req.body.price}},{new: true}, function(err,c){
-       if(err) console.log(err);
-       else{
-          res.redirect(`/teacher/${req.user._id}/courses`);
-       }
-     })
-    } else {
-      res.redirect("/login");
-    }
-  }
-);
-
-
-app.post("/teacher/:course_id/update-course-thumbnail",
+app.get("/view-course/:course_id", teacher.viewCourse);
+app.post("/:user_id/verify", upload.single("identification"), teacher.verifyPOST);
+app.post("/update/:user_id", profile.updateProfile);
+app.post("/teacher/:course_id/update-course-name",teacher.updateCourseNamePOST);
+app.post("/teacher/:course_id/update-course-desc", teacher.updateCourseDescPOST);
+app.post("/teacher/:course_id/update-course-price", teacher.updateCoursePricePOST);
+app.post(
+  "/teacher/:course_id/update-course-thumbnail",
   upload.fields([{ name: "thumbnail", maxCount: 1 }]),
-  function (req, res) {
-    console.log(req.body)
-    if (req.isAuthenticated()) {
-       Course.findOneAndUpdate({_id: req.params.course_id}, {$set: {image: req.files.thumbnail[0].filename}},{new: true}, function(err,c){
-       if(err) console.log(err);
-       else{
-          res.redirect(`/teacher/${req.user._id}/courses`);
-       }
-     })
-    } else {
-      res.redirect("/login");
-    }
-  }
+  teacher.updateCourseThumbPOST
 );
-
-app.post("/teacher/:course_id/add-course-video",
+app.post(
+  "/teacher/:course_id/add-course-video",
   upload.fields([{ name: "lecture" }]),
-  function (req, res) {
-    const s_id = req.params.course_id;
-    if (req.isAuthenticated()) {
-
-      let lecture_name = [];
-      req.files.lecture.forEach((ele) => {
-        lecture_name.push(ele.filename);
-      });
-
-      Course.findOne({_id: s_id}, function(err,c){
-          c.video.push({
-          $each: lecture_name,
-        });
-
-        c.save(function (err) {
-        if (!err) {
-          res.redirect(`/teacher/${req.user._id}/courses`);
-        } else {
-          console.log(err);
-        }
-      });
-      }) 
-    } else {
-      res.redirect("/login");
-    }
-  }
+  teacher.updateCourseVideoPOST
 );
-
-app.post("/teacher/:teach_id/add-course",
+app.post(
+  "/teacher/:teach_id/add-course",
   upload.fields([{ name: "thumbnail", maxCount: 1 }, { name: "lecture" }]),
-  function (req, res) {
-    const t_id = req.params.teach_id;
-    if (req.isAuthenticated()) {
-      const course = new Course({
-        title: req.body.title,
-        price: req.body.price,
-        description: req.body.desc,
-        image: req.files.thumbnail[0].filename,
-        teacher: req.user._id,
-        // video: req.files.lecture[0].filename,
-        teacherName: req.user.name,
-      });
-      let lecture_name = [];
-      req.files.lecture.forEach((ele) => {
-        lecture_name.push(ele.filename);
-      });
-      course.video.push({
-        $each: lecture_name,
-      });
-      course.save(function (err) {
-        if (!err) {
-          res.redirect(`/teacher/${t_id}/courses`);
-        } else {
-          console.log(err);
-        }
-      });
-    } else {
-      res.redirect("/login");
-    }
-  }
+  teacher.addCoursePOST
 );
 app.post("/login", login.loginPOST);
 app.post("/register", register.registerPOST);
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,32 @@
-const passport = require("passport")
-const User = require("../models/user")
-const mongoose = require("mongoose")
+const passport = require("passport");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
 exports.profileGET = (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("profile", { user: req.user });
-    } else res.redirect("/login");
-}
+  if (req.isAuthenticated()) {
+    res.render("profile", { user: req.user });
+  } else res.redirect("/login");
+};
 exports.editProfile = (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("edit-profile", { user: req.user });
-    } else res.redirect("/login");
-}
+  if (req.isAuthenticated()) {
+    res.render("edit-profile", { user: req.user });
+  } else res.redirect("/login");
+};
+exports.updateProfile = (req, res) => {
+  const newName = req.body.name;
+  const newEmail = req.body.email;
+  const ID = req.params.user_id;
+  if (req.isAuthenticated()) {
+    console.log(req);
+    User.updateOne(
+      { _id: ID },
+      { name: newName, username: newEmail },
+      function (err) {
+        if (!err) {
+          if (req.user.role === "teacher") res.redirect(`/teacher/${ID}`);
+          else if (req.user.role === "student") res.redirect(`/student/${ID}`);
+        }
+      }
+    );
+  }
+};
